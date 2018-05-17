0494cada4daab3aa181c16bf7af816279b028cc8
Ulrich Schmidt <support@ulicms.de>
2018-05-17 18:25:55 +0200

    fix table

--- src/content/modules/mail_queue/templates/admin.php
+++ src/content/modules/mail_queue/templates/admin.php
@@ -5,26 +5,29 @@ $queue = MailQueue::getInstance();
 $mails = $queue->getAllMails();
 ?>
 <table class="tablesorter">
-<thead>
-<tr>
-<th><?php translate("id");?></th>
-<th><?php translate("recipient");?></th>
-<th><?php translate("headers");?></th>
-<th><?php translate("message");?></th>
-<th><?php translate("created");?></th>
-</tr>
-</thead>
-<tbody>
-<?php foreach($mails as $mail){
+	<thead>
+		<tr>
+			<th><?php translate("id");?></th>
+			<th><?php translate("recipient");?></th>
+			<th><?php translate("headers");?></th>
+			<th><?php translate("message");?></th>
+			<th><?php translate("created");?></th>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+
+foreach ($mails as $mail) {
     ?>
 <tr>
-<td><?php esc($mail->getId());?></td>
-<td><?php esc($mail->getRecipient());?></td>
-<td><?php nl2br(esc($mail->getHeaders()));?></td>
-<td><?php nl2br(esc($mail->getMessage()));?></td>
-<td><?php esc(date('Y-m-d H:i:s', $mail->getCreated()));?></td>
-</tr>
+			<td><?php esc($mail->getId());?></td>
+			<td><?php esc($mail->getRecipient());?></td>
+			<td><?php echo nl2br(_esc($mail->getHeaders()));?></td>
+			<td><?php echo nl2br(_esc($mail->getMessage()));?></td>
+			<td><?php echo _esc(date('Y-m-d H:i:s', $mail->getCreated()));?></td>
+		</tr>
     <?php
 }
 ?>
-</tbody></table>
+</tbody>
+</table>
