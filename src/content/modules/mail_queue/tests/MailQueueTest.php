54024234d5e5b462cf79bc2c8bdaa49ba8359b9e
Ulrich Schmidt <u.schmidt@velian.de>
2018-10-24 14:22:52 +0200

    testfall gechrieben

--- src/content/modules/mail_queue/tests/MailQueueTest.php
+++ src/content/modules/mail_queue/tests/MailQueueTest.php
@@ -32,4 +32,27 @@ class MailQueueTest extends PHPUnit_Framework_TestCase {
 			$this->assertEquals($i - 1, count($queue->getAllMails()));
 		}	
 	}
+	// TODO: Kommentar schreiben
+	public function testGetNextMail(){
+		$queue = MailQueue\MailQueue::getInstance();
+		$mailIds = array();
+		for($i=1; $i <= 3; $i++){
+			$mail = new MailQueue\Mail();
+			$mail->setRecipient("receiver{$i}@example.org");
+			$mail->setSubject("Subject $i");
+			$mail->setMessage("Message $i");
+			$mail->setHeaders("From: foo@bar.de");
+			$queue->addMail($mail);
+			$mailsIds[] = $mail->getID();
+		}
+
+		$mail = $queue->getNextMail();
+		$this->assertEquals($mailIds[0], $mail->getID());
+		$mail = $queue->getNextMail();
+		$this->assertEquals($mailIds[1], $mail->getID());
+		$mail = $queue->getNextMail();
+		$this->assertEquals($mailIds[2], $mail->getID());
+	
+		$queue->flushMailQueue();
+	}
 }
