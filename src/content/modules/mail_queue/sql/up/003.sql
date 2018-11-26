b103a38e50bb85171a96e78628f2a57c46548d9e
Ulrich Schmidt <u.schmidt@velian.de>
2018-11-26 14:13:33 +0100

    general ci

--- src/content/modules/mail_queue/sql/up/003.sql
+++ src/content/modules/mail_queue/sql/up/003.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `{prefix}mail_queue` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `{prefix}mail_queue` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
